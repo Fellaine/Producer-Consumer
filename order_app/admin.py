4db6fb272c36fab5b866f41d0686c940f8d9e8c1
Fellaine <max.goncharevych@gmail.com>
2023-09-10 08:53:05 +0300

    Fixed Employee admin registration

--- order_app/admin.py
+++ order_app/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Employee, Order
 
 # Register your models here.
 
-admin.site.register([Employee, Order])
+
+class MyUserAdmin(UserAdmin):
+    model = Employee
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "probation",
+                    "position",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(Order)
+admin.site.register(Employee, MyUserAdmin)
